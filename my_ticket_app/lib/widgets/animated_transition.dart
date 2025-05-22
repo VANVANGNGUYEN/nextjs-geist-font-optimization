cbe6bad80eda0da55f0e8e7170b3442235afb5af
VANVANGNGUYEN <125632351+VANVANGNGUYEN@users.noreply.github.com>
2025-05-22 18:50:02 +0700

    Add my_ticket_app/lib/widgets/animated_transition.dart 1747914600584

---
+++ my_ticket_app/lib/widgets/animated_transition.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AnimatedTransition extends PageRouteBuilder {
+  final Widget page;
+
+  AnimatedTransition({required this.page})
+      : super(
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(1.0, 0.0);
+            const end = Offset.zero;
+            const curve = Curves.ease;
+
+            final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            final offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: child,
+            );
+          },
+        );
+}
