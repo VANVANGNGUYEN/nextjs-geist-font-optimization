44cc6cdb4a82d9534074fc07ea3967355923c0a0
VANVANGNGUYEN <125632351+VANVANGNGUYEN@users.noreply.github.com>
2025-05-22 18:49:58 +0700

    Add my_ticket_app/lib/utils/constants.dart 1747914596493

---
+++ my_ticket_app/lib/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1E40AF);
+  static const secondary = Color(0xFF2563EB);
+  static const background = Color(0xFFF9FAFB);
+  static const textPrimary = Color(0xFF111827);
+  static const textSecondary = Color(0xFF6B7280);
+}
+
+class AppImages {
+  static const backgroundImageUrl = 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg';
+  // Add more image URLs or local asset paths here
+}
